098a5cf7b7427cfd27bd956ab32071cdf7d6efe6
Bilou <jblin.prog@gmail.com>
2020-08-05 13:15:39 +0200

    premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-fluid">
+    <header-top class="row" id="header"></header-top>
+    <div class="row">
+      <connexion id="connexion"></connexion>
+    </div>
+    <div class="row">
+      <Navbar class="col-12  col-md-3 col-lg-2" id="navbar"></Navbar>
+      <div class="col-12 col-md-9 col-lg-10 px-5 pt-3" id="contenu">
+        <router-view></router-view>
+      </div>
+    </div>
+    
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/navbar/Navbar.vue'
+import Header from './components/header/Header.vue'
+import Connexion from './components/Connexion.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Navbar,
+    'header-top': Header,
+    'connexion': Connexion,
+  },
 }
 </script>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
+#navbar {
+  margin-top: 75px;
+  margin-left: 0px;
+  margin-right: 0px;
+  min-height: 100vh;
+}
+#header {
+  position: fixed;
+  height: 75px;
+  z-index: 1;
+  width: 100%;
+}
+#contenu {
+  margin-top : 75px;
+}
+
+#connexion {
+  margin-top : 75px;
+}
+
+@import './assets/variables.css';
+@import './style.css';
 </style>
